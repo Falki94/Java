155b897680c86b44cdec492c19c29c429c5e164b
Falki94 <49191159+Falki94@users.noreply.github.com>
2019-04-17 11:55:38 +0200

    Delete TestEmployee.java

--- employee/TestEmployee.java
+++
@@ -1,9 +0,0 @@
-package employee;
-
-public class TestEmployee {
-    public static void main(String[] args){
-        Employee e = new Employee(0,"Andrzej", "Andrzejewski", 10000);
-        System.out.println("Employee ID: " + e.getId() + "\nEmployee name: " + e.getFirstName() + "\nEmployee Surname: " + e.getLastName() + "\nSalary: "+ e.getSalary());
-    }
-}
-
