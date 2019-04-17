3e485111bcb5a8b06de4f48e6b87926d700eb1b7
Falki94 <49191159+Falki94@users.noreply.github.com>
2019-04-17 11:55:53 +0200

    Delete TestInvoiceItem.java

--- invoiceitem/TestInvoiceItem.java
+++
@@ -1,9 +0,0 @@
-package invoiceitem;
-
-
-public class TestInvoiceItem {
-    public static void main (String[] args){
-        InvoiceItem x = new InvoiceItem("fe34598123","paliwo",5,30);
-        System.out.println("InvoiceItem ID: "+ x.getId() + "\nDescription: "+ x.getDesc() + "\nQuantity: " + x.getQty() + "\nUnit Price: " + x.getUnitPrice());
-    }
-}
