1127c5504f9bd64aac1ad1a98b6336df262206b7
Falki94 <49191159+Falki94@users.noreply.github.com>
2019-04-17 18:47:08 +0200

    Add files via upload

---
+++ employee/TestEmployee.java
@@ -0,0 +1,9 @@
+package employee;
+
+public class TestEmployee {
+    public static void main(String[] args){
+        Employee e = new Employee(0,"Andrzej ", "Andrzejewski", 10000);
+        System.out.println(e.toString());
+    }
+}
+
