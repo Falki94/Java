b7dc091200da6eebf7772dee0b81d964e00b2e37
Falki94 <49191159+Falki94@users.noreply.github.com>
2019-05-01 20:05:56 +0200

    Update TestEmployee.java

--- Semester 2/POJ_03(04.06)/employee/TestEmployee.java
+++ Semester 2/POJ_03(04.06)/employee/TestEmployee.java
@@ -1,5 +1,8 @@
 package employee;
-
+/**
+ *
+ * @author Falki
+ */
 public class TestEmployee {
     public static void main(String[] args){
         Employee e = new Employee(0,"Andrzej ", "Andrzejewski", 10000);
