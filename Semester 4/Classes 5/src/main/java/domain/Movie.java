1780e1de517c4fe3f48bbff9d0468ac996bb31ad
Falki94 <49191159+Falki94@users.noreply.github.com>
2019-05-04 11:38:57 +0200

    Add files via upload

---
+++ Semester 4/Classes 5/src/main/java/domain/Movie.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.List;
+
+public class Movie {
+
+    private int id;
+    private String film;
+    private List<Actor> actors;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFilm() {
+        return film;
+    }
+
+    public void setFilm(String film) {
+        this.film = film;
+    }
+
+
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+}
