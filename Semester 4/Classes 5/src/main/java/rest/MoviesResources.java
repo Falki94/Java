9f01c5e5010619dddf2f42383ad5ffccb60df899
Falki94 <49191159+Falki94@users.noreply.github.com>
2019-05-04 12:08:43 +0200

    Update MoviesResources.java

--- Semester 4/Classes 5/src/main/java/rest/MoviesResources.java
+++ Semester 4/Classes 5/src/main/java/rest/MoviesResources.java
@@ -14,6 +14,10 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+/**
+ *
+ * @author Falki
+ */
 
 @Path("/movies")
 public class MoviesResources {
