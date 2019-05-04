70e53890e2526021e384f6a889efc3e1b60dbc08
Falki94 <49191159+Falki94@users.noreply.github.com>
2019-05-04 12:08:26 +0200

    Update ApplicationConfig.java

--- Semester 4/Classes 5/src/main/java/rest/ApplicationConfig.java
+++ Semester 4/Classes 5/src/main/java/rest/ApplicationConfig.java
@@ -3,6 +3,10 @@ package rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ *
+ * @author Falki
+ */
 @ApplicationPath("rest")
 public class ApplicationConfig extends Application{
 
