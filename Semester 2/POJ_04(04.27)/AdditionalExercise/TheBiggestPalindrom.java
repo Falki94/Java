329478da4ec83baa90a354c1d676ab352f755c8e
Falki94 <49191159+Falki94@users.noreply.github.com>
2019-05-11 16:28:43 +0200

    Update TheBiggestPalindrom.java

--- Semester 2/POJ_04(04.27)/AdditionalExercise/TheBiggestPalindrom.java
+++ Semester 2/POJ_04(04.27)/AdditionalExercise/TheBiggestPalindrom.java
@@ -1,12 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *The Longest palindrome of string
  */
 package AdditionalExercise;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -17,21 +13,32 @@ public class TheBiggestPalindrom {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        List<String> list = new ArrayList<>();
-        String linia = sc.nextLine();
-        linia = linia.toLowerCase();
-        String[] sa = linia.split("( )|(\\,)|(\\.)|(\\?)|(\\!)|(\\;)|(\\:)");
-        for (String s : sa){
-            if (s.isEmpty()){
-                continue;
+        String word = sc.nextLine();
+        System.out.println(theLongestPalindrome(word));
+    }
+
+    public static String theLongestPalindrome(String s) {
+
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = s.length() - 1; j >= 0 && j != i; j--) {
+                if (isPalindromowo(s.substring(i, j + 1))) {
+                    if (s.substring(i, j + 1).length() > result.length()) {
+                        result = s.substring(i, j + 1);
+                    }
+                }
             }
-            StringBuilder sb = new StringBuilder(s);
-            if (sb.reverse().toString().equals(s)){
-                list.add(s);
-            }
-            
         }
-        list.stream().sorted();
-        System.out.println(list.get(0));
+        return result;
+    }
+
+    public static boolean isPalindromowo(String x) {
+        x = x.toLowerCase();
+        for (int i = 0, j = x.length() - 1; i <= x.length() / 2; i++, j--) {
+            if (x.charAt(i) != x.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
